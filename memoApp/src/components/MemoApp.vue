9c2aeaa1e9aaf1cff1be0dd31da980b90e231f45
caesiumy <mn0658@naver.com>
2020-02-06 21:33:47 +0900

    feat: Add MemoForm component

---
+++ memoApp/src/components/MemoApp.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="memo-app">
+    <memo-form />
+    <memo />
+  </div>
+</template>
+
+<script>
+import Memo from "./Memo";
+import MemoForm from "./MemoForm";
+
+export default {
+  name: "MemoApp",
+  components: {
+    MemoForm
+  },
+  data() {
+    return {
+      memos: []
+    };
+  },
+  created() {
+    this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : [];
+  }
+};
+</script>
+
+<style scoped></style>
