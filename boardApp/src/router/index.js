7a3d95bfcb02cc162ee76c72f816990dcdd23cf6
CaesiumY <mn0658@naver.com>
2020-02-13 11:49:18 +0900

    feat: Add PostListPage basic template

--- boardApp/src/router/index.js
+++ boardApp/src/router/index.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import PostListPage from "@/pages/PostListPage";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "PostListPage",
+      component: PostListPage
     }
   ]
-})
+});
