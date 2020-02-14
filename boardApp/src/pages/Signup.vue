e9586b7293d61324796400d54d09d38d405fa13a
CaesiumY <mn0658@naver.com>
2020-02-14 10:03:30 +0900

    feat: Add structure of onSubmit method

--- boardApp/src/pages/Signup.vue
+++ boardApp/src/pages/Signup.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="sign-up-page">
     <h3>회원가입</h3>
-    <signup-form />
+    <signup-form @submit="onSubmit" />
   </div>
 </template>
 
 <script>
 import SignupForm from "../components/SignupForm";
 export default {
-  name: "회원가입",
+  name: "Signup",
   components: {
     SignupForm
+  },
+  methods: {
+    onSubmit(payload) {
+      console.log(payload);
+    }
   }
 };
 </script>
