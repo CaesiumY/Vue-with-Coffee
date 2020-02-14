c0fb6a8473d6c3ae0cf146062bbf094178d4bcb3
CaesiumY <mn0658@naver.com>
2020-02-14 09:57:05 +0900

    feat: Add SignupForm component

--- boardApp/src/pages/Signup.vue
+++ boardApp/src/pages/Signup.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="sign-up-page">
     <h3>회원가입</h3>
+    <signup-form />
   </div>
 </template>
 
 <script>
+import SignupForm from "../components/SignupForm";
 export default {
-  name: "회원가입"
+  name: "회원가입",
+  components: {
+    SignupForm
+  }
 };
 </script>
 
