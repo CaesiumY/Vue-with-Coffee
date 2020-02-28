f28f6fbf79236407644b662d4b2852078a1c94f7
caesiumy <mn0658@naver.com>
2020-02-28 21:48:03 +0900

    feat: Add PostCreateForm component

--- boardApp/src/pages/PostCreatePage.vue
+++ boardApp/src/pages/PostCreatePage.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="post-create-page">
     <h1>게시물 작성하기</h1>
+    <post-create-form @submit="onSubmit" />
   </div>
 </template>
 
 <script>
+import PostCreateForm from "../components/PostCreateForm";
 export default {
-  name: "PostCreatePage"
+  name: "PostCreatePage",
+  components: {
+    PostCreateForm
+  },
+  methods: {
+    onSubmit(payload) {
+      console.log(payload);
+      const { title, contents } = payload;
+    }
+  }
 };
 </script>
 
