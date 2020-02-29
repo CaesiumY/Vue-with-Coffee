477dcf4bbe484cd0ca159275cf9bd004a6fd60d6
caesiumy <mn0658@naver.com>
2020-02-29 18:26:11 +0900

    feat: Add PostEditForm component

--- boardApp/src/pages/PostEditPage.vue
+++ boardApp/src/pages/PostEditPage.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="post-edit-page">
     <h1>게시물 수정</h1>
+    <post-edit-form v-if="post" :post="post" @submit="onSubmit" />
+    <p v-else>게시물 불러오는 중...</p>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
+import PostEditForm from "../components/PostEditForm";
 
 export default {
   name: "PostEditPage",
@@ -15,8 +18,16 @@ export default {
       required: true
     }
   },
+  components: {
+    PostEditForm
+  },
   computed: {
     ...mapState(["post"])
+  },
+  methods: {
+    onSubmit(payload) {
+      console.log(payload);
+    }
   }
 };
 </script>
