bd145d327be27e34591f92aea87755cb06e0e65f
caesiumy <mn0658@naver.com>
2020-03-03 22:44:40 +0900

    feat: Add CommentItem component

---
+++ boardApp/src/components/CommentItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="comment-item">
+    <strong>{{ comment.user.name }}</strong>
+    <span>{{ comment.createdAt }}</span>
+    <p>{{ comment.contents }}</p>
+    <ul>
+      <li><button type="button">수정</button></li>
+      <li><button type="button">삭제</button></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommentItem",
+  props: {
+    comment: {
+      type: Object,
+      required: true,
+      validator(comment) {
+        const isValidCommentId = typeof comment.id === "number";
+        const isValidContents = comment.contents && comment.contents.length;
+        const isValidUser = !!comment.user;
+        return isValidCommentId && isValidContents && isValidUser;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- boardApp/src/components/CommentList.vue
+++ boardApp/src/components/CommentList.vue
@@ -1,17 +1,15 @@
 <template>
   <ul class="comments">
     <li v-for="comment in comments" :key="comment.id">
-      <div class="comment-item">
-        <strong>{{ comment.user.name }}</strong
-        ><span>{{ comment.createdAt }}</span>
-        <p>{{ comment.contents }}</p>
-      </div>
+      <comment-item :comment="comment" />
     </li>
     <li v-if="comments.length <= 0">입력된 댓글이 없습니다.</li>
   </ul>
 </template>
 
 <script>
+import CommentItem from "./CommentItem";
+
 export default {
   name: "CommentList",
   props: {
@@ -21,6 +19,9 @@ export default {
         return [];
       }
     }
+  },
+  components: {
+    CommentItem
   }
 };
 </script>
